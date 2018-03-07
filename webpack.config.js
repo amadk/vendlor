c80f147016c0e87f499990520fdd9d5ccfb88f8a
Mach13 <amad_khan1@hotmail.com>
2018-03-07 21:17:47 +0400

    Improve webpack for production and remove bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
+var CompressionPlugin = require('compression-webpack-plugin');
 
-module.exports = {
+var config = {
   entry: path.resolve(__dirname, 'client/src/index.js'),
   output: {
     path: path.resolve(__dirname, 'client/assets/build'),
@@ -16,24 +17,33 @@ module.exports = {
       },
       { test: /\.css$/, loader: 'style-loader!css-loader' } 
     ]
-  },
-  // plugins: [
-  //   new webpack.DefinePlugin({ //<--key to reduce React's size
-  //     'process.env': {
-  //       'NODE_ENV': JSON.stringify('production')
-  //     }
-  //   }),
-  //   new webpack.optimize.UglifyJsPlugin(),
-  //   new webpack.optimize.AggressiveMergingPlugin(),
-  //   new CompressionPlugin({
-  //     asset: "[path].gz[query]",
-  //     algorithm: "gzip",
-  //     test: /\.js$|\.css$|\.html$/,
-  //     threshold: 10240,
-  //     minRatio: 0.8
-  //   })
-  // ],
-  // node: {
-  //   __dirname: true,
-  // }
+  }
 };
+
+var plugins = {
+  plugins: [
+    new webpack.DefinePlugin({ //<--key to reduce React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ],
+  node: {
+    __dirname: true,
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config = Object.assign(config, plugins)
+}
+
+module.exports = config;
